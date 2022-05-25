fc31aa728bce1df71e372b0357ebe4d85e2c5cca
Kritika Kharbanda <60071917+krkh-1702@users.noreply.github.com>
2022-05-25 12:58:42 -0400

    started pages

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,3 +10,23 @@ $(window).scroll(function () {
         $(".bi-arrow-up-square-fill").fadeOut();
     }
 });
+
+
+// Video aspect ratio maintainer
+var j = 0;
+const video_width = $(".comp_video").width()
+comp_video_array = document.getElementsByClassName('comp_video')
+for (j = 0; j < comp_video_array.length; j++) {
+    comp_video_array[j].style.height = (9 / 16) * video_width + "px";
+}
+// Document ready - means the page is loaded
+$(document).ready(function () {
+    $(window).resize(function () {
+        var j = 0;
+        const video_width = $(".comp_video").width()
+        comp_video_array = document.getElementsByClassName('comp_video')
+        for (j = 0; j < comp_video_array.length; j++) {
+            comp_video_array[j].style.height = (9 / 16) * video_width + "px";
+        }
+    });
+});
